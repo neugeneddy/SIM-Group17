75c16c4376e48a1821da41e9e4ba5216e0e3855a
Suson Maharjan <163854703+neugeneddy@users.noreply.github.com>
2025-03-17 15:28:26 +0545

    Add files via upload

---
+++ R-Stroke Dataset/r.R
@@ -0,0 +1,5 @@
+install.packages(c("rmarkdown", "knitr", "xfun", "tinytex"))
+tinytex::install_tinytex()  # Installs a LaTeX distribution
+
+install.packages("rmarkdown")
+rmarkdown::pandoc_version()
